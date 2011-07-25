997c843c8434640eb44188543d406e6492f5312c
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-25 10:08:30 +0200

    Break up Gust.parse
      * Created Gust.image, .markdown, .textile and .plain_text
      * Extended Pygments to use highlight instead of Pygments.highlight

--- lib/gust.rb
+++ lib/gust.rb
@@ -3,16 +3,39 @@ require 'redcarpet'
 require 'RedCloth'
 
 class Gust
+  extend Pygments
 
-  def self.parse(code, options = {})
-    return "<div class=\"highlight\"><pre>#{code}</pre></div>\n" unless options[:filename]
+  class << self
 
-    case options[:filename]
-      when /.*\.png$/ then "<div class=\"image\"><img src=\"#{options[:url]}\"/></div>\n"
-      when /.*\.markdown$/, /.*\.md$/ then "<div class=\"markup\">#{Redcarpet.new(code).to_html}</div>\n"
-      when /.*\.textile$/ then "<div class=\"markup\">#{RedCloth.new(code).to_html}</div>\n"
-      else Pygments.highlight(code, options) rescue "<div class=\"highlight\"><pre>#{code}</pre></div>\n"
+    def parse(code, options = {})
+      return plain_text(code) unless options[:filename]
+
+      case options[:filename]
+        when /.*\.png$/ then image(options[:url])
+        when /.*\.markdown$/, /.*\.md$/ then markdown(code)
+        when /.*\.textile$/ then textile(code)
+        else highlight(code, options) rescue plain_text(code)
+      end
+
     end
+
+    protected
+
+      def image(src)
+        "<div class=\"image\"><img src=\"#{src}\"/></div>\n"
+      end
+
+      def markdown(markup)
+        "<div class=\"markup\">#{Redcarpet.new(markup).to_html}</div>\n"
+      end
+
+      def textile(markup)
+        "<div class=\"markup\">#{RedCloth.new(markup).to_html}</div>\n"
+      end
+
+      def plain_text(text)
+        "<div class=\"highlight\"><pre>#{text}</pre></div>\n"
+      end
 
   end
 
